923a68408fd19fcc22983a23429b2204117d8602
SuprDewd <suprdewd@gmail.com>
2010-09-06 21:43:49 +0000

    Added a 'IsPortFree' method to BagNet which checks if the specified port is free. Also updated the class diagram.

--- SharpBag/BagNet/BagNet.cs
+++ SharpBag/BagNet/BagNet.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.NetworkInformation;
+using System.Linq;
 
 namespace SharpBag.BagNet
 {
@@ -7,6 +9,16 @@ namespace SharpBag.BagNet
     /// </summary>
     public static class BagNet
     {
+        /// <summary>
+        /// Checks whether the specified port number is valid and not in use.
+        /// </summary>
+        /// <param name="port">The port.</param>
+        /// <returns>Whether the specified port number is valid and not in use.</returns>
+        public static bool IsPortFree(int port)
+        {
+            if (port < 0 || port > 0xFFFF) return false;
 
+            return (from i in IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpConnections() where i.LocalEndPoint.Port == port select true).Count() == 0;
+        }
     }
 }
