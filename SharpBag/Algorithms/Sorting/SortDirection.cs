cbc309c39c678ab150d9b7aaa5dfcc0a20a51a27
SuprDewd <suprdewd@gmail.com>
2010-11-14 10:37:52 +0000

    Added a BubbleSort extension.

---
+++ SharpBag/Algorithms/Sorting/SortDirection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpBag.Algorithms.Sorting
+{
+    /// <summary>
+    /// The order to sort in.
+    /// </summary>
+    public enum SortOrder
+    {
+        /// <summary>
+        /// Sort ascending.
+        /// </summary>
+        Ascending,
+        /// <summary>
+        /// Sort descending.
+        /// </summary>
+        Descending
+    }
+}
---
+++ SharpBag/Algorithms/Sorting/SortExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpBag.Algorithms.Sorting
+{
+    public static class SortExtensions
+    {
+        /// <summary>
+        /// BubbleSorts the specified instance.
+        /// </summary>
+        /// <typeparam name="T">The type of the items in the current instance.</typeparam>
+        /// <param name="collection">The current instance.</param>
+        /// <param name="order">The order to sort in.</param>
+        /// <returns>The sorted collection.</returns>
+        public static IEnumerable<T> BubbleSort<T>(this IEnumerable<T> collection, SortOrder order = SortOrder.Ascending) where T : IComparable<T>
+        {
+            T[] array = collection.ToArray();
+            T temp;
+
+            for (int outer = array.Length - 1; outer >= 1; outer--)
+            {
+                for (int inner = 0; inner <= outer - 1; inner++)
+                {
+                    if (array[inner].CompareTo(array[inner + 1]) == (order == SortOrder.Ascending ? 1 : -1))
+                    {
+                        temp = array[inner];
+                        array[inner] = array[inner + 1];
+                        array[inner + 1] = temp;
+                    }
+                }
+            }
+
+            return array;
+        }
+    }
+}
