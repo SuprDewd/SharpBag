b3f63a7ce89ae89de06f43259f4d147c7364a114
SuprDewd <suprdewd@gmail.com>
2011-06-10 12:42:47 +0000

    Files back to their correct places.

--- SharpBag/Math/Calculators/Calculator.cs
+++ SharpBag/Math/Calculators/Calculator.cs
@@ -4,73 +4,232 @@ using System.Linq;
 using System.Numerics;
 using System.Text;
 
-namespace SharpBag.Math
+namespace SharpBag.Math.Calculators
 {
+	/// <summary>
+	/// A generic calculator.
+	/// </summary>
+	/// <typeparam name="T">The type to calculate.</typeparam>
 	public abstract class Calculator<T>
 	{
 		#region Operations
 
+		/// <summary>
+		/// Adds the specified numbers.
+		/// </summary>
+		/// <param name="a">The first number.</param>
+		/// <param name="b">The second number.</param>
+		/// <returns>The added numbers.</returns>
 		public abstract T Add(T a, T b);
 
+		/// <summary>
+		/// Subtracts the specified numbers.
+		/// </summary>
+		/// <param name="a">The first number.</param>
+		/// <param name="b">The second number.</param>
+		/// <returns>The subtracted numbers.</returns>
 		public virtual T Subtract(T a, T b)
 		{
 			return Add(a, Negate(b));
 		}
 
+		/// <summary>
+		/// Multiplies the specified numbers.
+		/// </summary>
+		/// <param name="a">The first number.</param>
+		/// <param name="b">The second number.</param>
+		/// <returns>The multiplied numbers.</returns>
 		public abstract T Multiply(T a, T b);
 
+		/// <summary>
+		/// Divides the specified numbers.
+		/// </summary>
+		/// <param name="a">The first number.</param>
+		/// <param name="b">The second number.</param>
+		/// <returns>The divided numbers.</returns>
 		public abstract T Divide(T a, T b);
 
+		/// <summary>
+		/// Calculates the remainder of the first number divided by the second number.
+		/// </summary>
+		/// <param name="a">The first number.</param>
+		/// <param name="b">The second number.</param>
+		/// <returns>The remainder of the first number divided by the second number..</returns>
 		public abstract T Modulo(T a, T b);
 
+		/// <summary>
+		/// Negates the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The negated number.</returns>
 		public abstract T Negate(T n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract T Convert(byte n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract byte ConvertToByte(T n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract T Convert(short n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract short ConvertToShort(T n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract T Convert(ushort n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract ushort ConvertToUShort(T n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract T Convert(int n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract int ConvertToInt(T n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract T Convert(uint n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract uint ConvertToUInt(T n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract T Convert(long n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract long ConvertToLong(T n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract T Convert(ulong n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract ulong ConvertToULong(T n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract T Convert(float n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract float ConvertToFloat(T n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract T Convert(double n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract double ConvertToDouble(T n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract T Convert(decimal n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract decimal ConvertToDecimal(T n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract T Convert(BigInteger n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract BigInteger ConvertToBigInteger(T n);
 
+		/// <summary>
+		/// Converts the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The converted number.</returns>
 		public abstract T Convert(string n);
 
+		/// <summary>
+		/// Converts the number to string.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The string.</returns>
 		public virtual string ConvertToString(T n)
 		{
 			return n.ToString();
@@ -80,55 +239,135 @@ namespace SharpBag.Math
 
 		#region Functions
 
+		/// <summary>
+		/// Calculates the ceiling of the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The ceiling.</returns>
 		public abstract T Ceiling(T n);
 
+		/// <summary>
+		/// Calculates the floor of the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The floor.</returns>
 		public abstract T Floor(T n);
 
+		/// <summary>
+		/// Raises the number to the specified power.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <param name="power">The power.</param>
+		/// <returns>The number raised to the specified power.</returns>
 		public abstract T Pow(T n, T power);
 
+		/// <summary>
+		/// Calculates the greatest common divisor of the numbers.
+		/// </summary>
+		/// <param name="a">The first number.</param>
+		/// <param name="b">The second number.</param>
+		/// <returns>The greatest common divisor.</returns>
 		public abstract T Gcd(T a, T b);
 
+		/// <summary>
+		/// Calculates the least common multiple of the numbers.
+		/// </summary>
+		/// <param name="a">The first number.</param>
+		/// <param name="b">The second number.</param>
+		/// <returns>The least common multiple.</returns>
 		public abstract T Lcm(T a, T b);
+
+		/// <summary>
+		/// Calculates the square root of the number.
+		/// </summary>
+		/// <param name="n">The number.</param>
+		/// <returns>The square root.</returns>
+		public abstract T Sqrt(T n);
 
 		#endregion Functions
 
 		#region Comparisons
 
+		/// <summary>
+		/// Checks whether the first number is greater than the second number.
+		/// </summary>
+		/// <param name="a">The first number.</param>
+		/// <param name="b">The second number.</param>
+		/// <returns>The result.</returns>
 		public virtual bool GreaterThan(T a, T b)
 		{
 			return Compare(a, b) > 0;
 		}
 
+		/// <summary>
+		/// Checks whether the first number is greater than or equal to the second number.
+		/// </summary>
+		/// <param name="a">The first number.</param>
+		/// <param name="b">The second number.</param>
+		/// <returns>The result.</returns>
 		public virtual bool GreaterThanOrEqual(T a, T b)
 		{
 			return Compare(a, b) >= 0;
 		}
 
+		/// <summary>
+		/// Checks whether the first number is less than the second number.
+		/// </summary>
+		/// <param name="a">The first number.</param>
+		/// <param name="b">The second number.</param>
+		/// <returns>The result.</returns>
 		public virtual bool LessThan(T a, T b)
 		{
 			return Compare(a, b) < 0;
 		}
 
+		/// <summary>
+		/// Checks whether the first number is less than or equal to the second number.
+		/// </summary>
+		/// <param name="a">The first number.</param>
+		/// <param name="b">The second number.</param>
+		/// <returns>The result.</returns>
 		public virtual bool LessThanOrEqual(T a, T b)
 		{
 			return Compare(a, b) <= 0;
 		}
 
+		/// <summary>
+		/// Checks whether the first number is equal to the second number.
+		/// </summary>
+		/// <param name="a">The first number.</param>
+		/// <param name="b">The second number.</param>
+		/// <returns>The result.</returns>
 		public virtual bool Equal(T a, T b)
 		{
 			return Compare(a, b) == 0;
 		}
 
+		/// <summary>
+		/// Compares the first number to the second number.
+		/// </summary>
+		/// <param name="a">The first number.</param>
+		/// <param name="b">The second number.</param>
+		/// <returns>The result.</returns>
 		public abstract int Compare(T a, T b);
 
 		#endregion Comparisons
 
 		#region Constants
 
+		/// <summary>
+		/// A one.
+		/// </summary>
 		public abstract T One { get; }
 
+		/// <summary>
+		/// A negative one.
+		/// </summary>
 		public abstract T NegativeOne { get; }
 
+		/// <summary>
+		/// A zero.
+		/// </summary>
 		public abstract T Zero { get; }
 
 		#endregion Constants
---
+++ SharpBag/Math/Calculators/CheckedCalculator.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace SharpBag.Math.Calculators
+{
+	/// <summary>
+	/// A calculator with all operations checked for overflow.
+	/// </summary>
+	/// <typeparam name="T">The type to calculate.</typeparam>
+	public class CheckedCalculator<T> : Calculator<T>
+	{
+		/// <summary>
+		/// The unchecked calculator.
+		/// </summary>
+		public Calculator<T> Calculator { get; private set; }
+
+		/// <summary>
+		/// The constructor.
+		/// </summary>
+		/// <param name="calculator">The calculator to use.</param>
+		public CheckedCalculator(Calculator<T> calculator)
+		{
+			this.Calculator = calculator;
+		}
+
+		public override T Add(T a, T b)
+		{
+			checked
+			{
+				return this.Calculator.Add(a, b);
+			}
+		}
+
+		public override T Subtract(T a, T b)
+		{
+			checked
+			{
+				return this.Calculator.Subtract(a, b);
+			}
+		}
+
+		public override T Multiply(T a, T b)
+		{
+			checked
+			{
+				return this.Calculator.Multiply(a, b);
+			}
+		}
+
+		public override T Divide(T a, T b)
+		{
+			checked
+			{
+				return this.Calculator.Divide(a, b);
+			}
+		}
+
+		public override T Modulo(T a, T b)
+		{
+			checked
+			{
+				return this.Calculator.Modulo(a, b);
+			}
+		}
+
+		public override T Negate(T n)
+		{
+			checked
+			{
+				return this.Calculator.Negate(n);
+			}
+		}
+
+		public override T Convert(byte n)
+		{
+			checked
+			{
+				return this.Calculator.Convert(n);
+			}
+		}
+
+		public override byte ConvertToByte(T n)
+		{
+			checked
+			{
+				return this.Calculator.ConvertToByte(n);
+			}
+		}
+
+		public override T Convert(short n)
+		{
+			checked
+			{
+				return this.Calculator.Convert(n);
+			}
+		}
+
+		public override short ConvertToShort(T n)
+		{
+			checked
+			{
+				return this.Calculator.ConvertToShort(n);
+			}
+		}
+
+		public override T Convert(ushort n)
+		{
+			checked
+			{
+				return this.Calculator.Convert(n);
+			}
+		}
+
+		public override ushort ConvertToUShort(T n)
+		{
+			checked
+			{
+				return this.Calculator.ConvertToUShort(n);
+			}
+		}
+
+		public override T Convert(int n)
+		{
+			checked
+			{
+				return this.Calculator.Convert(n);
+			}
+		}
+
+		public override int ConvertToInt(T n)
+		{
+			checked
+			{
+				return this.Calculator.ConvertToInt(n);
+			}
+		}
+
+		public override T Convert(uint n)
+		{
+			checked
+			{
+				return this.Calculator.Convert(n);
+			}
+		}
+
+		public override uint ConvertToUInt(T n)
+		{
+			checked
+			{
+				return this.Calculator.ConvertToUInt(n);
+			}
+		}
+
+		public override T Convert(long n)
+		{
+			checked
+			{
+				return this.Calculator.Convert(n);
+			}
+		}
+
+		public override long ConvertToLong(T n)
+		{
+			checked
+			{
+				return this.Calculator.ConvertToLong(n);
+			}
+		}
+
+		public override T Convert(ulong n)
+		{
+			checked
+			{
+				return this.Calculator.Convert(n);
+			}
+		}
+
+		public override ulong ConvertToULong(T n)
+		{
+			checked
+			{
+				return this.Calculator.ConvertToULong(n);
+			}
+		}
+
+		public override T Convert(float n)
+		{
+			checked
+			{
+				return this.Calculator.Convert(n);
+			}
+		}
+
+		public override float ConvertToFloat(T n)
+		{
+			checked
+			{
+				return this.Calculator.ConvertToFloat(n);
+			}
+		}
+
+		public override T Convert(double n)
+		{
+			checked
+			{
+				return this.Calculator.Convert(n);
+			}
+		}
+
+		public override double ConvertToDouble(T n)
+		{
+			checked
+			{
+				return this.Calculator.ConvertToDouble(n);
+			}
+		}
+
+		public override T Convert(decimal n)
+		{
+			checked
+			{
+				return this.Calculator.Convert(n);
+			}
+		}
+
+		public override decimal ConvertToDecimal(T n)
+		{
+			checked
+			{
+				return this.Calculator.ConvertToDecimal(n);
+			}
+		}
+
+		public override T Convert(BigInteger n)
+		{
+			checked
+			{
+				return this.Calculator.Convert(n);
+			}
+		}
+
+		public override BigInteger ConvertToBigInteger(T n)
+		{
+			checked
+			{
+				return this.Calculator.ConvertToBigInteger(n);
+			}
+		}
+
+		public override T Convert(string n)
+		{
+			checked
+			{
+				return this.Calculator.Convert(n);
+			}
+		}
+
+		public override string ConvertToString(T n)
+		{
+			return this.Calculator.ConvertToString(n);
+		}
+
+		public override int Compare(T a, T b)
+		{
+			checked
+			{
+				return this.Calculator.Compare(a, b);
+			}
+		}
+
+		public override bool Equal(T a, T b)
+		{
+			return this.Calculator.Equal(a, b);
+		}
+
+		public override bool GreaterThan(T a, T b)
+		{
+			return this.Calculator.GreaterThan(a, b);
+		}
+
+		public override bool GreaterThanOrEqual(T a, T b)
+		{
+			return this.Calculator.GreaterThanOrEqual(a, b);
+		}
+
+		public override bool LessThan(T a, T b)
+		{
+			return this.Calculator.LessThan(a, b);
+		}
+
+		public override bool LessThanOrEqual(T a, T b)
+		{
+			return this.Calculator.LessThanOrEqual(a, b);
+		}
+
+		public override T Ceiling(T n)
+		{
+			checked
+			{
+				return this.Calculator.Ceiling(n);
+			}
+		}
+
+		public override T Floor(T n)
+		{
+			checked
+			{
+				return this.Calculator.Floor(n);
+			}
+		}
+
+		public override T Pow(T n, T power)
+		{
+			checked
+			{
+				return this.Calculator.Pow(n, power);
+			}
+		}
+
+		public override T Gcd(T a, T b)
+		{
+			checked
+			{
+				return this.Calculator.Gcd(a, b);
+			}
+		}
+
+		public override T Lcm(T a, T b)
+		{
+			checked
+			{
+				return this.Calculator.Lcm(a, b);
+			}
+		}
+
+		public override T Sqrt(T n)
+		{
+			checked
+			{
+				return this.Calculator.Sqrt(n);
+			}
+		}
+
+		public override T One
+		{
+			get { return this.Calculator.One; }
+		}
+
+		public override T NegativeOne
+		{
+			get { return this.Calculator.NegativeOne; }
+		}
+
+		public override T Zero
+		{
+			get { return this.Calculator.Zero; }
+		}
+	}
+}
