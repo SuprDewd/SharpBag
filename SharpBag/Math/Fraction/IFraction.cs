58225f0254799313296177b3f7a8e6b1a19e4f5f
SuprDewd <suprdewd@gmail.com>
2011-06-25 00:06:44 +0000

    Refactored the Fraction class.

---
+++ SharpBag/Math/Fraction/IFraction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpBag.Math.Fraction
+{
+	/// <summary>
+	/// A rational fraction.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public interface Fraction<T>
+	{
+		/// <summary>
+		/// The numerator.
+		/// </summary>
+		T Numerator { get; }
+
+		/// <summary>
+		/// The denominator.
+		/// </summary>
+		T Denominator { get; }
+
+		/// <summary>
+		/// Whether to automatically reduce the fraction.
+		/// </summary>
+		bool AutoReduce { get; }
+
+		/// <summary>
+		/// Object.ToString()
+		/// </summary>
+		/// <param name="digits">The maximum number of digits after the comma.</param>
+		/// <returns>The string representation of the fraction.</returns>
+		string ToString(int digits);
+	}
+}
